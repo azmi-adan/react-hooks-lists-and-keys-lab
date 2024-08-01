dac6ac221a0a00934aba955bd9286fa415c46ef2
azmi adan <azmi.adan2032@gmail.com>
2024-08-01 07:40:15 +0300

    add all

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,20 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const navItems = links.map((link, index) => (
+    <li key={index}>
+      <a href={`#${link}`}>{link}</a>
+    </li>
+  ));
+
+  return (
+    <nav>
+      <ul>
+        {navItems}
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
+
